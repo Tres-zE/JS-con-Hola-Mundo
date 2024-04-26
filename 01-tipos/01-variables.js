546ac0e58de822d74ff215972abb6f45e99dec18
rulo-dev <rulosan77@gmail.com>
2024-04-25 23:43:34 -0600

    reforzando js

---
+++ 01-tipos/01-variables.js
@@ -0,0 +1,9 @@
+//3 formas de escribir las varoables: UpperCamelCase, camelCase, snake_case
+//UpperCamelCase
+let NombreCompleto = 'Hola Mundo';
+//camelCase
+let nombreCompleto = 'Hola Mundo';
+//snake_case
+let nombre_completo = 'Hola Mundo';
+
+console.log(nombre);
