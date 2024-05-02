248f18f5830f287a563d514d8f7295a7c5b6530d
rulo-dev <rulosan77@gmail.com>
2024-05-01 23:51:12 -0600

    tipos de datos en js

---
+++ 01-tipos/02-primitivos.js
@@ -0,0 +1,7 @@
+let numero = 1;
+let texto = "Hola Mundo";
+let verdadero = true;
+let falso = false;
+let noDefinido;//undefined
+let undef = undefined;//puede ser un tipo de dato o un valor
+let nulo = null;
---
+++ 01-tipos/03-constantes.js
@@ -0,0 +1,7 @@
+const nombre = "Hola Mundo";
+nombre = "Chanchito Feliz"
+
+console.log(nombre)
+
+//const se usa para jamas cambiar el valor de una variable
+//let es por si alguna razon debemos cambair el valor de alguna varible
