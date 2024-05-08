dfb77a862ab095ba805e3b312e66661312d1bd90
rulo-dev <rulosan77@gmail.com>
2024-05-07 23:50:00 -0600

    operadores aritmeticos

---
+++ 02-operadores/01-aritmeticos.js
@@ -0,0 +1,18 @@
+let a = 5;
+let b = 7;
+
+// console.log(a + b, 'suma');//adición
+// console.log(a - b, 'resta');//resta
+// console.log(a * b, 'multiplicación');//multiplicación
+// console.log(a / b, 'divición');//divición
+// console.log(a % b, 'módulo');//modulo = residuo de una divicion
+// console.log(a ** b, 'potencia');//potencia
+
+// incrementar
+console.log(++a);
+console.log(a++);
+console.log(a);
+// decrementar
+console.log(--a);
+console.log(a--);
+console.log(a);
