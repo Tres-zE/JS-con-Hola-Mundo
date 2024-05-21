ec8bd514e114ea207127c300034b2885777c6b60
rulo-dev <rulosan77@gmail.com>
2024-05-20 23:23:56 -0600

    opeador de comparacion

---
+++ 02-operadores/03-comparacion.js
@@ -0,0 +1,13 @@
+//operadores relacionales
+let a = 10;
+console.log(a > 5);
+console.log(a >=5);
+console.log(a < 5);
+console.log(a <= 5);
+
+//operadores de igualdad
+console.log(a == 10);
+console.log(a != 10);
+console.log(a == '10');//se compara el valor y no el tipo de dato
+console.log(a === '10');//se compara el valor y el tipo el tipo de dato
+console.log(a !== '10');
---
+++ 02-operadores/04-logicos.js
@@ -0,0 +1,17 @@
+// AND, OR , NOT
+
+// AND--> &&
+console.log(true && true);
+console.log(false && true);
+
+let mayor = false;
+let suscrito = true;
+console.log('operador AND', mayor && suscrito);
+
+//OR--> ||
+console.log('operador OR', mayor || suscrito);
+
+//NOT--> !
+console.log('operador NOT', !mayor);
+let catalogoInfantil = !mayor;
+console.log('operador NOT', catalogoInfantil);
