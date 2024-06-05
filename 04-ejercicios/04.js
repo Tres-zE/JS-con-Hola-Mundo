265c8df63f36c8c2c40339baf2bc6036eaa16f67
rulo-dev <rulosan77@gmail.com>
2024-06-04 23:52:37 -0600

    numeros impares

---
+++ 04-ejercicios/04.js
@@ -0,0 +1,15 @@
+//imprimir numeros impares del 1 al 10 con while
+let i = 0;
+while (i <= 10) {
+    if (i % 2 !== 0) {
+        console.log('impar ', i);
+    }
+    i++;
+}
+
+//imprimir numeros impares del 1 al 10 con for
+for (let j = 0; j <= 10; j++) {
+    if (j % 2 !== 0) {
+        console.log('impar ', j);
+    }
+}
